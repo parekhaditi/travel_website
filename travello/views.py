6e30e2437eb5c5bae4e3043ce8a5b33f674d8d9b
Aditi Parekh <aditisatishparekh@gmail.com>
2020-07-14 22:08:14 +0530

    first commit

---
+++ travello/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+
+
+def index(request):
+
+    dests = Destination.objects.all()
+
+    return render(request, "templates/index.html", {'dests': dests})
+
+
+
+
